e24c012c03443d521a417eeb9102681563774b45
dkdam <dkdam@live.com>
2023-01-25 17:06:26 +1100

    add post

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-const UserModel = require('./models/Users')
+const UserModel = require('./models/Users');
+
+const cors = require('cors'); // Prevent errors in the client side
+
+app.use(express.json()) //This is important for parsing the object data through
+app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URL);
 
@@ -15,8 +20,16 @@ app.get("/getUsers", (req, res) => {
             res.json(result)
         }
     });
-});
+}); // Show Data
+
+app.post("/createUser", async (req,res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user)
+}); // Create Data
 
 app.listen(3001, () => {
-    console.log("SERVER RUNS FINE");
-})
+    console.log("SERVER IS RUNNING!");
+}) //Testing the server
