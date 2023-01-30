0f61cf133719c2aea875bf61e131e3b2d2567d48
dkdam <dkdam@live.com>
2023-01-30 13:54:31 +1100

    setted up api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers").then((response) => {
+      setListOfUsers(response.data);
+    })
+  },[])
 
   return (
     <div className="App">
       <h1>
-        Hello, is it me your looking for?
+        <div className='userDisplay'>
+          {listOfUsers.map((user) => 
+          {return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Age: {user.age}</h1>
+              <h1>Username: {user.username}</h1>
+            </div>
+          )
+          })}
+        </div>
       </h1>
     </div>
   )
