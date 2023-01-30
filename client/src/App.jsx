d6ceebc4ec707117480b2dd5aa9618f94ada6c5e
dkdam <dkdam@live.com>
2023-01-30 14:24:45 +1100

    full stack works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,36 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 import Axios from 'axios';
 
 function App() {
   const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("")
+  const [age, setAge] = useState(0)
+  const [username, setUsername] = useState("")
 
   useEffect(() => {
     Axios.get("http://localhost:3001/getUsers").then((response) => {
       setListOfUsers(response.data);
     })
-  },[])
+  },[]);
+
+  const createUser = () => {
+    Axios.post("http://localhost:3001/createUser", {
+      name: name, 
+      age: age, 
+      username: username,
+    }).then((response) => [
+      setListOfUsers([...listOfUsers, {
+        name,
+        age,
+        username,
+      }])
+    ]);
+  };
 
   return (
     <div className="App">
-      <h1>
+      <div>
         <div className='userDisplay'>
           {listOfUsers.map((user) => 
           {return (
@@ -25,7 +42,14 @@ function App() {
           )
           })}
         </div>
-      </h1>
+      </div>
+
+      <div>
+        <input type="text" placeholder="Name..." onChange={(e) => {setName(e.target.value)}} />
+        <input type="number" placeholder="Age..." onChange={(e) => {setAge(e.target.value)}} />
+        <input type="username" placeholder="Username..." onChange={(e) => {setUsername( e.target.value)}} />
+        <button onClick={createUser}>Create User</button>
+      </div>
     </div>
   )
 }
